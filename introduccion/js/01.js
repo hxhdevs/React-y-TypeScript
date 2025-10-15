240df16e6269adf1c8178450476c9b80fc3ba6e1
HXHdevs <jordisonnicolas@gmail.com>
2025-10-15 13:00:24 -0600

    Variables con let

---
+++ introduccion/js/01.js
@@ -0,0 +1,8 @@
+let cliente = "Juan"
+cliente = 30 //reasignando valor
+
+let precio
+precio = 1000
+console.log(cliente,precio)
+
+// Las variables de tipo let te permiten reasignar valores si no se definio desde el inicio o bien hacerlo desde que se declaran

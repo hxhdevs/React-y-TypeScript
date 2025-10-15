38330b0028b665bef38b192e1b564ea756d65dee
HXHdevs <jordisonnicolas@gmail.com>
2025-10-15 13:05:30 -0600

    Variables con const

---
+++ introduccion/js/02.js
@@ -0,0 +1,8 @@
+const cliente = "Juan"
+cliente = 30
+
+const precio = 1000
+console.log(precio)
+
+//Las variables de tipo const deben ser inicializadas desde que se declaran y no se puede reasignar el valor de esta
+// a diferencia de las let que se pueden reasignar el valor de la variable

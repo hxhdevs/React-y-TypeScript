412254926cbe02980fa83c8b5210c172a8243505
HXHdevs <jordisonnicolas@gmail.com>
2025-12-11 14:49:47 -0600

    Recorriendo arrays con for

---
+++ introduccion/js/12.js
@@ -0,0 +1,11 @@
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js', 'Nest.js', 'TypeScript']
+
+// for(let i = 0; i < tecnologias.length; i++) {
+//     console.log(tecnologias[i] )
+// }
+
+// forEach
+// tecnologias.forEach(function(tech) {
+//     console.log(tech)
+// })
+

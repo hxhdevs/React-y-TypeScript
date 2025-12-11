d256a7e57a1e8a38bebc68fffaad5855a1bca5d2
HXHdevs <jordisonnicolas@gmail.com>
2025-12-11 15:00:20 -0600

    Recorriendo arrays con forEach map y for ... of

--- introduccion/js/12.js
+++ introduccion/js/12.js
@@ -9,3 +9,18 @@ const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js', 'Nest.j
 //     console.log(tech)
 // })
 
+// map 
+const arrayMap = tecnologias.map(function(tech) {
+    return tech
+})
+
+// for ... of aqui ejecuta un bloque de codigo por cada elemento del arreglo
+for(let tech of tecnologias) {
+    console.log(tech)
+}
+
+// console.log(arrayMap)
+
+// ASKIN: diferencia entre map y foreach
+// map genera un nuevo array en base a la funcion o condicion dada y foreach solamente ejecuta una funcion dada por la cantidad de veces que tenga el arreglo
+//  va sobre del mismoarray ya existente
